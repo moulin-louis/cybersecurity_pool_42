88b5d864ce7934fdad663c8e80f08df95785c43e
Louis Moulin <louis.moulin@outlook.fr>
2023-09-25 14:29:36 +0200

    update iron_dome

--- iron_dome/src/crypto_activity.rs
+++ iron_dome/src/crypto_activity.rs
@@ -7,7 +7,6 @@ pub fn detect_crypto_activity(watcher: &mut Watcher) {
     for cpu in watcher.system_info.cpus() {
         avrg_load += cpu.cpu_usage() as u32;
     }
-    println!("{}%", avrg_load / watcher.system_info.cpus().len() as u32);
     avrg_load /= watcher.system_info.cpus().len() as u32;
     if avrg_load > 90 {
         println!("Intense cpu activiy detected: {}%!", avrg_load);
