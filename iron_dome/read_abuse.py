78177d814c20bb0ca1979b0f40981470168758c7
Louis Moulin <louis.moulin@outlook.fr>
2023-09-25 16:27:53 +0200

    update iron_dome

--- iron_dome/read_abuse.py
+++ iron_dome/read_abuse.py
@@ -1,13 +1,10 @@
 import threading
 
-def read_dev_zero_in_chunks(chunk_size=1024*1024, limit=1024*1024*10):  # Default limit is 10MB
-    total_read = 0
-    with open('/dev/zero', 'rb') as f:
-        while total_read < limit:
-            chunk = f.read(chunk_size)
-            total_read += len(chunk)
-            # Simulate processing, or simply discard the chunk to abuse disk reads
-            del chunk
+def read_dev_zero_in_chunks(chunk_size=1024*1024, limit=1024*1024*10):
+    while True:
+        with open('/dev/zero', 'rb') as f:
+                chunk = f.read(chunk_size)
+                del chunk
 
 def spawn_readers(num_threads=5, chunk_size=1024*1024, limit=1024*1024*10):
     threads = []
