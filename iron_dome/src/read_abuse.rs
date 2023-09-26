b855eb9d991dfac6c4a31c9438916ddc9ba27fe6
Louis Moulin <louis.moulin@outlook.fr>
2023-09-26 13:19:48 +0200

    update iron_dome

--- iron_dome/src/read_abuse.rs
+++ iron_dome/src/read_abuse.rs
@@ -1,9 +1,9 @@
 use std::{path::PathBuf, process};
 use std::fs::read;
 use crate::watcher::Watcher;
-use sysinfo::{ProcessExt, SystemExt, DiskUsage, Pid, PidExt};
+use sysinfo::{ProcessExt, SystemExt, Pid, PidExt};
 
-const THRESHOLD_READ: u64 = 100000000; //100MB
+const THRESHOLD_READ: u64 = 1000000; //1MB
 
 fn read_file(path: PathBuf) -> Result<Vec<u8>, i32> {
     match read(&path) {
@@ -18,21 +18,18 @@ fn read_file(path: PathBuf) -> Result<Vec<u8>, i32> {
     }
 }
 
-fn check_abuse(pid: &Pid, disk_usage: &DiskUsage) {
+fn check_abuse(pid: &Pid, read_since_last_time: u64) {
     let pid_nbr: u32 = pid.as_u32();
     if process::id() == pid_nbr{
         return;
     }
-    if disk_usage.read_bytes != 0 {
-        println!("Found a potential candidate");
-    }
-    if disk_usage.read_bytes > THRESHOLD_READ {
+    if read_since_last_time > THRESHOLD_READ {
         let mut path_pid: String = String::from("/proc/");
         path_pid.push_str(pid_nbr.to_string().as_str());
         path_pid.push_str("/comm");
         let mut name_process: String = String::from_utf8(read_file(PathBuf::from(path_pid)).unwrap()).unwrap();
         name_process.remove(name_process.len() - 1);
-        println!("Potential read abuse : [{}]:[{}] -> {} bytes read",pid, name_process, disk_usage.read_bytes);
+        println!("Potential read abuse : [{}]:[{}] -> {} bytes read",pid, name_process, read_since_last_time);
     }
 }
 
@@ -40,7 +37,6 @@ pub fn detect_disk_read_abuse(watcher: &mut Watcher) {
     watcher.system_info.refresh_processes();
     watcher.system_info.refresh_disks();
     for (pid, process) in watcher.system_info.processes() {
-        let disk_usage: DiskUsage = process.disk_usage();
-        check_abuse(pid, &disk_usage);
+        check_abuse(pid, process.disk_usage().read_bytes);
     }
 }
