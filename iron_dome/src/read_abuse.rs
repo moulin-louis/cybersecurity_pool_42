5d95c41cbd45461467bd484d8c46b46c57314db1
Louis Moulin <louis.moulin@outlook.fr>
2023-09-26 14:10:35 +0200

    update iron_dome

--- iron_dome/src/read_abuse.rs
+++ iron_dome/src/read_abuse.rs
@@ -62,7 +62,7 @@ pub fn detect_disk_read_abuse(watcher: &mut Watcher) {
             mb_wrtn: fields[9].parse::<f64>().unwrap() / 2048.0,
         };
         if !fields[3].contains("loop") {
-            println!("disk {}, read: {}, write:{}", fields[4], ds.mb_read, ds.mb_wrtn)
+            println!("disk {}, read: {}, write:{}", fields[2], ds.mb_read, ds.mb_wrtn)
             }
         if watcher.disk_read.contains_key(fields[2]) {
             curr.insert(fields[2].to_owned(), ds);
