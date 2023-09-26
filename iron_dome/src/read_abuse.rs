de7da14d1fb2c5b1eeca19e9b67e032231546ea5
Louis Moulin <louis.moulin@outlook.fr>
2023-09-26 14:49:42 +0200

    update iron_dome

--- iron_dome/src/read_abuse.rs
+++ iron_dome/src/read_abuse.rs
@@ -33,6 +33,6 @@ pub fn detect_disk_read_abuse(watcher: &mut Watcher) {
     fd.seek(SeekFrom::Start(0)).unwrap();
     watcher.disk_read = curr;
     for (name, read) in &watcher.disk_read {
-        println!("disk {}, read: {} sector so {} bytes", name, read, read * 512);
+        println!("disk {}, read: {} sector so {} MB", name, read, (read * 512) / 2048);
     }
 }
