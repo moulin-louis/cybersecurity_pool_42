2a5ac1964a3dbdd58d214564cdd9882b13976519
Louis Moulin <louis.moulin@outlook.fr>
2023-10-02 11:04:47 +0200

    update iron_dome

--- iron_dome/test/read_test.py
+++ iron_dome/test/read_test.py
@@ -1,15 +1,25 @@
 import multiprocessing
-import os
 
 def read_large_file_in_chunks():
-    chunk_size= 1024*1024
+    chunk_size=1024*1024
     while True:
-        os.system('echo 1234 | su -c "echo 1 > /proc/sys/vm/drop_caches"')
-        total_read = 0
+        os.system('echo 1234 | su -c "echo 1 > /proc/sys/vm/drop_caches"')total_read = 0
         with open ("./bigfile", "rb") as f:
             while (byte := f.read(chunk_size)):
                 total_read += len(byte)
                 del byte
         print('bytes read:', total_read)
 
-read_large_file_in_chunks()
+
+def spawn_readers(num_processes):
+    chunk_size=1024*1024
+    processes = []
+    for _ in range(num_processes):
+        p = multiprocessing.Process(target=read_large_file_in_chunks, args=())
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+spawn_readers(30)
