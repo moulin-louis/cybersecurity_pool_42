5e1623012a02d9e03c8c72f894560e2f8d66d539
Louis Moulin <louis.moulin@outlook.fr>
2023-10-02 12:07:01 +0200

    update iron_dome

--- iron_dome/test/entropy_test.sh
+++ iron_dome/test/entropy_test.sh
@@ -2,10 +2,11 @@
 while true;
   echo 'creating file'
   echo "Bonjour" > file_test
-  sleep 1
+  sleep 4
   echo 'changing file'
   echo "fsdjklfdskj3k2j4lj23kl4j23;;.;;,;,;//../.34fggdfgjgdfklhjKLGJKSDJFKGJmv,cm,vmxcn" > file_test
   echo 'reseting file'
+  sleep 4
   echo "Bonjour" > file_test
   echo 'deleting file'
   rm file_test
