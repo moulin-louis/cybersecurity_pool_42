c440438b4aa8ce3764a61a2e81cd77f99c5ceb02
Louis Moulin <louis.moulin@outlook.fr>
2023-10-02 11:01:48 +0200

    update iron_dome

--- iron_dome/test/read_test.py
+++ iron_dome/test/read_test.py
@@ -4,7 +4,7 @@ import os
 def read_large_file_in_chunks():
     chunk_size= 1024*1024
     while True:
-        os.system('echo 1 > /proc/sys/vm/drop_caches')
+        os.system('echo 1234 | su -c "echo 1 > /proc/sys/vm/drop_caches"')
         total_read = 0
         with open ("./bigfile", "rb") as f:
             while (byte := f.read(chunk_size)):
