1236f368d533c198d4d997a306104cd02d99ab96
Louis Moulin <louis.moulin@outlook.fr>
2023-10-02 10:48:49 +0200

    update iron_dome

--- iron_dome/test/read_test.py
+++ iron_dome/test/read_test.py
@@ -2,9 +2,12 @@ import multiprocessing
 
 def read_large_file_in_chunks(filename, chunk_size=1024*1024, limit=1024*1024*10):  # Default limit is 10MB
    while True:
+        total_read = 0
         with open ("./bigfile", "rb") as f:
             while (byte := f.read(chunk_size)):
+                total_read += len(byte)
                 del byte
+        print('bytes read:', total_read)
 
 
 def spawn_readers(num_processes, filename='bigfile', chunk_size=1024*1024, limit=1024*1024*10):
